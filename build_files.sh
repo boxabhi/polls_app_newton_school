6bd25fbafef8bb3ab172e4d53b38599396f84aca
= <abhijeetg40@gmail.com>
2022-11-21 19:02:07 +0530

    d

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
 # build_files.sh
-pip3 install -r requirements.txt
-# python3.9 manage.py collectstatic --noinput --clear
+echo "Building start"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
