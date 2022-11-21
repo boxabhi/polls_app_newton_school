dbeb02e05d971b669aca6502f7a05f9baaea899d
= <abhijeetg40@gmail.com>
2022-11-21 19:34:56 +0530

    done vercel setting

--- poll/settings.py
+++ poll/settings.py
@@ -110,9 +110,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
--- poll/wsgi.py
+++ poll/wsgi.py
@@ -15,4 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll.settings')
 
 application = get_wsgi_application()
 app = application
-
